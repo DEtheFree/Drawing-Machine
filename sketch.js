7ba0e00908a68542f141b6f2c436de19701a47bd
DEtheFree <diegoe2017@gmail.com>
2020-10-28 19:25:29 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ strokeWidth = noise(noiseOffset) * 100;
 stroke(map(mouseX, 0, 600, 0, 255, true))
 line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
 line(mouseX, mouseY, pmouseX, pmouseY);
-
+line(mouseY, mouseX, pmouseY, pmouseX)
 }
 
 
